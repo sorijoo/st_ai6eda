9693fb3effce8063f3dabd43d64c053171a9bb57
sorijoo <48428325+sorijoo@users.noreply.github.com>
2022-07-02 16:15:36 +0900

    Update Model_1.py

--- pages/Model_1.py
+++ pages/Model_1.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import pandas as pd 
 import numpy as np 
-from html_module import section, callout, line_break, title
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -77,120 +76,124 @@ from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify = y, test_size = 0.2, random_state = 42)
 
 
+
+# st.write(X_train)
+# st.write(X_test)
+
+
 ## 모델 선택 - 로지스틱 리그레션
 
-section('Logistic Regression', 300)
+st.write("## Logistic Regression")
 col1, col2 = st.columns(2)
 with col1:
-    # metric = st.radio('metric', ['mae', 'mse', 'rmse'])
+  
     penalty = st.selectbox('Penalty', ('l2', 'none'))
-    line_break()
+    st.markdown("<br/>", unsafe_allow_html=True)
     c = float(st.slider('C', 0.0, 100.0, 10.0))
 
 with col2:
     solver = st.selectbox('Solver', ('newton-cg','lbfgs', 'liblinear','sag','saga'))
-    line_break()
+    st.markdown("<br/>", unsafe_allow_html=True)
     max_iter = float(st.slider('Max iter', 500.0, 10000.0, 7000.0))
  
 
-line_break()
+st.markdown("<br/>", unsafe_allow_html=True)
 
 model_lr = st.button('Logistic Regression 모델링 Start')
 
 if model_lr:
-    # lr_params = set_lr_params(penalty, c, solver, max_iter)
+ 
     lr = LogisticRegression(penalty=penalty, C=c, solver=solver, max_iter=max_iter)
 
-    # my_bar = st.progress(0)
     lr_model_state = st.text('인생을 예측하는 데에는 시간이 걸립니다')
     lr_model = lr.fit(X_train, y_train)
-    # for percent_complete in range(100):
-    # time.sleep(0.1)
-    # my_bar.progress(percent_complete + 1)
-    lr_model_state.success("모델링 완료")
+
+    lr_model_state.success("완료!")
 
     y_predict = lr_model.predict(X_test)
     score = scoreModel(lr_model, X_train, X_test, y_train, y_test)
-    st.metric("이 모델의 정확도는 : ", round(score,3))
+    st.metric("이 모델의 정확도는 : ", round(score,5))
+    if score < 0.7 :
+        st.image("data/cat_meme.jpeg")
+    else:
+        st.image("data/ai.jpeg")    
    
 
 ## 모델 선택 - 랜덤 포레스트
 
-section('Random Forest', 300)
+st.write("## Random Forest")
 col1, col2 = st.columns(2)
 with col1:
-    # metric = st.radio('metric', ['mae', 'mse', 'rmse'])
-    n_estimators = int(st.slider('N estimators', 5, 30, 10))
-    line_break()
+    n_estimators = int(st.slider('N estimators', 5, 500, 300))
+    st.markdown("<br/>", unsafe_allow_html=True)
     min_samples_split = int(st.slider('Min samples split', 1, 30, 10))
-    line_break()
+    st.markdown("<br/>", unsafe_allow_html=True)
     min_samples_leaf = float(st.slider('Min samples leaf', 0.0, 0.5, 0.3))
 
 
 with col2:
     max_depth= int(st.slider('Max depth', 1, 10, 3))
-    line_break()
+    st.markdown("<br/>", unsafe_allow_html=True)
     max_leaf_nodes= int(st.slider('Max leaf nodes', 1, 10, 3))
  
 
-line_break()
+st.markdown("<br/>", unsafe_allow_html=True)
 
 model_rf = st.button('Random Forest 모델링 Start')
 
 if model_rf:
-    # lr_params = set_lr_params(penalty, c, solver, max_iter)
+
     rf = RandomForestClassifier(n_estimators=n_estimators, min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf, max_depth=max_depth, max_leaf_nodes=max_leaf_nodes)
 
-    # my_bar = st.progress(0)
     rf_model_state = st.text('인생을 예측하는 데에는 시간이 걸립니다')
     rf_model = rf.fit(X_train, y_train)
-    # for percent_complete in range(100):
-    # time.sleep(0.1)
-    # my_bar.progress(percent_complete + 1)
-    rf_model_state.success("모델링 완료")
+
+    rf_model_state.success("완료!")
 
     y_predict = rf_model.predict(X_test)
     score = scoreModel(rf_model, X_train, X_test, y_train, y_test)
-    st.metric("이 모델의 정확도는 : ", round(score,3))
+    st.metric("이 모델의 정확도는 : ", round(score,5))
+    if score < 0.7 :
+        st.image("data/cat_meme.jpeg")
+    else:
+        st.image("data/ai.jpeg")    
 
 
 
 #세번째 모델
 
-section('K-Nearest Neighbors', 300)
+st.write("## K-Nearest Neighbors")
 col1, col2 = st.columns(2)
 with col1:
-    # metric = st.radio('metric', ['mae', 'mse', 'rmse'])
     n_neighbors = int(st.slider('N neighbors', 2, 30, 5))
-    line_break()
+    st.markdown("<br/>", unsafe_allow_html=True)
     weights = st.selectbox('Weight', ('uniform', 'distance'))
     
 
 
 with col2:
     leaf_size= int(st.slider('Max depth', 1, 100, 30))
-    line_break()
+    st.markdown("<br/>", unsafe_allow_html=True)
     algorithm = st.selectbox('Algorithm', ('auto', 'ball_tree', 'kd_tree', 'brute'))
  
 
-line_break()
+st.markdown("<br/>", unsafe_allow_html=True)
 
 model_knn = st.button('K-Nearest Neighbors 모델링 Start')
 
 if model_knn:
-    # lr_params = set_lr_params(penalty, c, solver, max_iter)
+    
     knn = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights, leaf_size=leaf_size, algorithm=algorithm)
-    # my_bar = st.progress(0)
     knn_model_state = st.text('인생을 예측하는 데에는 시간이 걸립니다')
     knn_model = knn.fit(X_train, y_train)
-    # for percent_complete in range(100):
-    # time.sleep(0.1)
-    # my_bar.progress(percent_complete + 1)
-    knn_model_state.success("모델링 완료")
-
+    knn_model_state.success("완료!")
     y_predict = knn_model.predict(X_test)
     score = scoreModel(knn_model, X_train, X_test, y_train, y_test)
-    st.metric("이 모델의 정확도는 : ", round(score,3))
+    st.metric("이 모델의 정확도는 : ", round(score,5))
+    if score < 0.7 :
+        st.image("data/cat_meme.jpeg")
+    else:
+        st.image("data/ai.jpeg")    
 
 
 # clf=RandomForestClassifier()
